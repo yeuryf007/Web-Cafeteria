2a4e4ab093a94411808e3a2f618d33f3cc98c41d
Yeury Paula <YFPO0001@ce.pucmm.edu.do>
2023-03-21 20:54:51 -0400

    html del nav bar

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="css/stylehome.css">
+    <script src="https://kit.fontawesome.com/87238b4970.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <div>Logo Empresa</div>
+        <nav>
+        <a href="index.php"><button>Home</button></a>
+        <a href="reservas.php"><button>Reservas</button></a>
+        <a href="menu.php"><button>Menú</button></a>
+        <a href="miperfil.php"><button>Mi perfil</button></a>
+        <!--
+            el enlace del boton de ingreso se pega al boton de carrito, hay que hacerle hice al boton de carrito hasta que alguien ingrese
+        -->
+        <a href="ingreso.php">
+            <button>
+                <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+                Ingreso
+            </button>
+        </a>
+        <a href="carrito.php">
+            <button>
+                <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+                Carrito
+            </button>
+        </a>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+
+</body>
+    <footer>
+        
+    </footer>
+</html>
