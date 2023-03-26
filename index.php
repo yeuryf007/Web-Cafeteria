b7a3205324016759c908a720ccbfcf46e55c93ca
Yeury Paula <YFPO0001@ce.pucmm.edu.do>
2023-03-26 00:26:49 -0400

    index html casi acabado

--- index.php
+++ index.php
@@ -27,18 +27,37 @@
         </a>
         <a href="carrito.php">
             <button>
-                <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+                <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
                 Carrito
             </button>
         </a>
         </nav>
+        <div>
+            <h1>Cafeteria Deluxe</h1>
+            <p>La vida es como una taza de café tú eliges si Io quieres dulce o amargo, frio o caliente, solo o acompañado.</p>
+            <a href="menu.php"><button>Menú</button></a>
+            <a href="reservas.php"><button>Reservas</button></a>
+        </div>
     </header>
     <main>
-
+        <img src="/imagenes/granos.png" alt=""></img>
+        <div>
+            <img src="/imagenes/granos.png" alt=""></img>
+            <h2>Acerca de</h2>
+            <p>Bienvenido a xd cafe, su lugar favorito para disfrutar de comida deliciosa y fresca en un ambiente acogedor. Estamos dedicados a proporcionar a nuestros clientes comidas de alta calidad que sean nutritivas y satisfactorias.En xd cafe, creemos que la comida debe ser deliciosa y saludable. Es por eso que obtenemos nuestros ingredientes de granjas y proveedores locales siempre que sea posible, y hacemos todo desde cero en nuestra cocina. Nuestro menú cambia regularmente para reflejar las estaciones y mostrar los mejores ingredientes disponibles.</p>
+        </div>
     </main>
-
 </body>
     <footer>
-        
+        <img src="/imagenes/cafeteriafooter.jpg" alt=""></img>
+        <div></div>
+        <nav>
+            <a href="index.php"><button>Home</button></a>
+            <a href="reservas.php"><button>Reservas</button></a>
+            <a href="menu.php"><button>Menú</button></a>
+            <a href="miperfil.php"><button>Mi perfil</button></a>
+        </nav>
+        <div></div>
+        <div></div>
     </footer>
 </html>
