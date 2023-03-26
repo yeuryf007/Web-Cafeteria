9802d28eb9dffd62a6ccb0c7723588ec89be8db2
Yeury Paula <YFPO0001@ce.pucmm.edu.do>
2023-03-26 03:01:51 -0400

    arreglos main

--- index.php
+++ index.php
@@ -45,9 +45,11 @@
         </div>
     </header>
     <main>
-            <img src="imagenes/granos.png" alt=""></img>
+        <img id="mostrador" src="imagenes/mostrador-cafeteria.jpg" alt=""></img>
+            
         <div>
-            <h2>Acerca de</h2>
+            <h2>Acerca de<img id="granos" src="imagenes/granos.png" alt=""></img></h2>
+            
             <p>Bienvenido a xd cafe, su lugar favorito para disfrutar de comida deliciosa y fresca en un ambiente acogedor. Estamos dedicados a proporcionar a nuestros clientes comidas de alta calidad que sean nutritivas y satisfactorias. <br><br> En xd cafe, creemos que la comida debe ser deliciosa y saludable. Es por eso que obtenemos nuestros ingredientes de granjas y proveedores locales siempre que sea posible, y hacemos todo desde cero en nuestra cocina. Nuestro menú cambia regularmente para reflejar las estaciones y mostrar los mejores ingredientes disponibles.</p>
         </div>
     </main>
