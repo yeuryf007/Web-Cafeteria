84a3ac037f416207f8e11a80ed16d3449496919a
numerotres865 <rubentgonzalez@gmail.com>
2023-03-26 07:26:14 -0400

    Ahora si footer en php arreglado

--- footer.php
+++ footer.php
@@ -9,17 +9,17 @@
             <a href="reservas.php">Reservas</a>
             <a href="menu.php">Menú</a>
             <a href="miperfil.php">Mi perfil</a>
-        </div>
+</div>
         <div>
             <h3>Contacto</h3>
-            <a href=""><i class="fa-solid fa-envelope" style="color: #ffffff;"></i> Correoverdadero@mail.com</a>
-            <p><i class="fa-solid fa-phone" style="color: #ffffff;"></i> 123-456-7891</p>
+            <a href=""><i class="fa-solid fa-envelope" style="color: #ffffff;"></i> Correo@correo.com</a>
+            <p><i class="fa-solid fa-phone" style="color: #ffffff;"></i> 123456789</p>
             <p><i class="fa-solid fa-location-dot" style="color: #ffffff;"></i> Calle freidy el profe, #el final</p>
         </div>
         <div>
             <h3>Redes sociales</h3>
-            <a href=""><i class="redes fa-brands fa-square-facebook" style="color: #ffffff;"></i></a>
-            <a href=""><i class="redes fa-brands fa-square-instagram" style="color: #ffffff;"></i></a>
-            <a href=""><i class="redes fa-brands fa-square-twitter" style="color: #ffffff;"></i></a>
+            <a href=""><i class="fa-brands fa-square-facebook" style="color: #ffffff;"></i></a>
+            <a href=""><i class="fa-brands fa-square-instagram" style="color: #ffffff;"></i></a>
+            <a href=""><i class="fa-brands fa-square-twitter" style="color: #ffffff;"></i></a>
         </div>
     </footer>
