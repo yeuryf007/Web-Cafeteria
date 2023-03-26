5814324e941151d916d0d0f6981ec017a7038c76
numerotres865 <rubentgonzalez@gmail.com>
2023-03-26 07:18:56 -0400

    footer arreglado en php

--- footer.php
+++ footer.php
@@ -3,23 +3,23 @@
             <h2>Visitanos aquí</h2>
             <!--Introducier google maps o imagen de ubicacion-->
         </div>
-        <nav>
+        <div>
             <h3>Navegación</h3> 
             <a href="index.php">Home</a>
             <a href="reservas.php">Reservas</a>
             <a href="menu.php">Menú</a>
             <a href="miperfil.php">Mi perfil</a>
-        </nav>
+        </div>
         <div>
             <h3>Contacto</h3>
-            <a href=""><i class="fa-solid fa-envelope" style="color: #ffffff;"></i> Correo@correo.com</a>
-            <p><i class="fa-solid fa-phone" style="color: #ffffff;"></i> 123456789</p>
+            <a href=""><i class="fa-solid fa-envelope" style="color: #ffffff;"></i> Correoverdadero@mail.com</a>
+            <p><i class="fa-solid fa-phone" style="color: #ffffff;"></i> 123-456-7891</p>
             <p><i class="fa-solid fa-location-dot" style="color: #ffffff;"></i> Calle freidy el profe, #el final</p>
         </div>
         <div>
             <h3>Redes sociales</h3>
-            <a href=""><i class="fa-brands fa-square-facebook" style="color: #ffffff;"></i></a>
-            <a href=""><i class="fa-brands fa-square-instagram" style="color: #ffffff;"></i></a>
-            <a href=""><i class="fa-brands fa-square-twitter" style="color: #ffffff;"></i></a>
+            <a href=""><i class="redes fa-brands fa-square-facebook" style="color: #ffffff;"></i></a>
+            <a href=""><i class="redes fa-brands fa-square-instagram" style="color: #ffffff;"></i></a>
+            <a href=""><i class="redes fa-brands fa-square-twitter" style="color: #ffffff;"></i></a>
         </div>
     </footer>
