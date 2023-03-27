c0e6ccfbae19396df86589512255b3f7683eb094
numerotres865 <rubentgonzalez@gmail.com>
2023-03-27 01:23:37 -0400

    header a php aparte

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
-
+<?php
+session_start();
+?>
     <header>
         <div class="topbar">    
             <div class="logo">Logo Empresa</div>
@@ -6,21 +8,27 @@
             <a href="index.php"><button class="boton1">Home</button></a>
             <a href="reservas.php"><button class="boton1">Reservas</button></a>
             <a href="menu.php"><button class="boton1">Menú</button></a>
-            <a href="miperfil.php"><button class="boton1">Mi perfil</button></a>
+            <?php
+            if (isset($_SESSION["user"])) {
+                echo "<a href='miperfil.php'><button class='boton1'>Mi perfil</button></a>";
+                echo "<a href='includes/logout.inc.php'><button class='boton1'>Log out</button></a>";
+            }
+            ?>
             <!--
                 el enlace del boton de ingreso se pega al boton de carrito, hay que hacerle hide al boton de carrito hasta que alguien ingrese
             -->
+            <a class="a-button" href="login.php">
+                <button class="boton2">
+                    <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
+                    Login
+                </button>
+            </a>
             <a class="a-button" href="signup.php">
                 <button class="boton2">
                     <i class="fa-solid fa-user" style="color: #ffffff;"></i>
-                    Ingreso 
+                    Registro 
                 </button>
             </a>
-            <a class="a-button" href="carrito.php">
-                <button class="boton2">
-                    <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
-                    Carrito
-                </button>
-            </a>
+            
             </nav>
         </div>
