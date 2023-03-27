8af4d54a79af385672ff40de8d7d7c116dfbb1a3
Youster920 <youssefcg1@gmail.com>
2023-03-27 03:11:46 -0400

    menu nuevo B)

--- menu.php
+++ menu.php
@@ -24,61 +24,71 @@
         
     </header>
     <main>
-        <div class="table">
-        <ul>
-            <li>
-                <h2>Entradas</h2>
-                <ul>
-                    <li>Ensalada de tomate y cebolla</li>
-                    <li>Ensalada de col y zanahoria</li>
-                    <li>Ensalada de lechuga y pepino</li>
-                    <li>Ensalada de espinaca y aguacate</li>
-                    <li>Ensalada de lechuga y tomate</li>
-                </ul>
-            </li>
-            <li>
-                <h2>Platos fuertes</h2>
-                <ul>
-                    <li>Pollo a la plancha</li>
-                    <li>Carne asada</li>
-                    <li>Filete de pescado</li>
-                    <li>Chuleta de cerdo</li>
-                    <li>Chuleta de res</li>
-                </ul>
-            </li>
-            <li>
-                <h2>Postres</h2>
-                <ul>
-                    <li>Brownie de chocolate</li>
-                    <li>Galletas de avena con pasas</li>
-                    <li>Helado de vainilla con caramelo</li>
-                </ul>
-            </li>
-        </ul> 
-        </div>
-        <div class="table">
-           <ul>
-            <li>
-                <h2>Bebidas</h2>
-                <ul>
-                    <li>Coca-Cola</li>
-                    <li>Sprite</li>
-                    <li>Fanta</li>
-                    <li>Agua</li>
-                    <li>Agua de jamaica</li>
-                </ul>
-            </li>
-            <li>
-                <h2>Extras</h2>
-                <ul>
-                    <li>Queso</li>
-                    <li>Champiñones</li>
-                    <li>Chile</li>
-                    <li>Crema</li>
-                    <li>Sal</li>
-                </ul>
-            </li>
-        </ul> 
+        <div id="cuerpo">
+            <div class="table">
+                <div class="menus">
+                    <img src="imagenes/ensalada.png">
+                    <h2>Entradas</h2>
+                    <ul>
+                        <li>Ensalada de tomate y cebolla</li>
+                        <li>Ensalada de col y zanahoria</li>
+                        <li>Ensalada de lechuga y pepino</li>
+                        <li>Ensalada de espinaca y aguacate</li>
+                        <li>Ensalada de lechuga y tomate</li>
+                    </ul>
+                </div>
+                    
+                <div class="menus">
+                <img src="imagenes/ribeye.png">
+                    <h2>Platos fuertes</h2>
+                        <ul>
+                            <li>Pollo a la plancha</li>
+                            <li>Carne asada</li>
+                            <li>Filete de pescado</li>
+                            <li>Chuleta de cerdo</li>
+                            <li>Chuleta de res</li>
+                            <li>Ojo de costilla</li>
+                        </ul>
+                </div>
+                <div class="menus">
+                <img src="imagenes/tarta fresa.png">
+                    <h2>Postres</h2>
+                        <ul>
+                            <li>Brownie de chocolate</li>
+                            <li>Galletas de avena con pasas</li>
+                            <li>Helado de vainilla con caramelo</li>
+                            <li>Cheesecake</li>
+                            <li>Tarta de fresa de chocolate blanco</li>
+                        </ul>
+                </div>
+            </div>
+            <div class="table">
+            <div class="menus">
+            <img src="imagenes/jugos.png">
+                    <h2>Bebidas</h2>
+                        <ul>
+                            <li>Jugo de naranja</li>
+                            <li>Jugo de piña</li>
+                            <li>Jugo de mango</li>
+                            <li>Jugo de papaya</li>
+                            <li>Jugo de uva</li>
+                        </ul>
+            </div>
+                
+                <div class="menus">
+                <img src="imagenes/crema.png">
+                    <h2>Extras</h2>
+                    <ul>
+                        <li>Queso</li>
+                        <li>Champiñones</li>
+                        <li>Chile</li>
+                        <li>Crema</li>
+                        <li>Sal</li>
+                    </ul>
+                </div>
+
+                
+            </div>
         </div>
     </main>
     <script>
