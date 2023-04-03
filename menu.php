f109d1f93dc8f090a06f2740bf145c2e6a7476d1
Yeury Paula <YFPO0001@ce.pucmm.edu.do>
2023-04-03 00:04:55 -0400

    patch a ordenar

--- menu.php
+++ menu.php
@@ -24,17 +24,226 @@
         <button id="ordenar">Ordenar</button>
     </header>
     <main>
-        
+<!--Esto es el ordenar-->
+        <div id="main2">
+            <nav>
+                <button id="entradas">Entradas</button>
+                <button id="platosfuertes">Platos fuertes</button>
+                <button id="postres">Postres</button>
+                <button id="bebidas">Bebidas</button>
+                <button id="extras">Extras</button>
+            </nav>
+            <div id="comidas-div">
+                <div id="entradasdiv">
+                    <div class="comida">
+                        <h2>Ensalada de tomate y cebolla</h2>
+                        <p>Ensalada de tomate y cebolla</p>
+                        <p>Precio<br>$50</p>
+                        <button id="entradas1">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Ensalada de col y zanahoria</h2>
+                        <p>Ensalada de col y zanahoria</p>
+                        <p>Precio<br>$50</p>
+                        <button id="entradas2">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Ensalada de lechuga y pepino</h2>
+                        <p>Ensalada de lechuga y pepino</p>
+                        <p>Precio<br>$50</p>
+                        <button id="entradas3">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Ensalada de espinaca y aguacate</h2>
+                        <p>Ensalada de espinaca y aguacate</p>
+                        <p>Precio<br>$50</p>
+                        <button id="entradas4">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Ensalada de lechuga y tomate</h2>
+                        <p>Ensalada de lechuga y tomate</p>
+                        <p>Precio<br>$50</p>
+                        <button id="entradas5">Agregar</button>
+                    </div>
+                </div>
+                <div id="platosfuertesdiv">
+                    <div class="comida">
+                        <h2>Pollo a la plancha</h2>
+                        <p>Pollo a la plancha</p>
+                        <p>Precio<br>$100</p>
+                        <button id="platosfuertes1">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Carne asada</h2>
+                        <p>Carne asada</p>
+                        <p>Precio<br>$100</p>
+                        <button id="platosfuertes2">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Filete de pescado</h2>
+                        <p>Filete de pescado</p>
+                        <p>Precio<br>$100</p>
+                        <button id="platosfuertes3">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Chuleta de cerdo</h2>
+                        <p>Chuleta de cerdo</p>
+                        <p>Precio<br>$100</p>
+                        <button id="platosfuertes4">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Chuleta de res</h2>
+                        <p>Chuleta de res</p>
+                        <p>Precio<br>$100</p>
+                        <button id="platosfuertes5">Agregar</button>
+                    </div>
+                </div>
+                <div id="postresdiv">
+                    <div class="comida">
+                        <h2>Brownie de chocolate</h2>
+                        <p>Brownie de chocolate</p>
+                        <p>Precio<br>$50</p>
+                        <button id="postres1">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Galletas de avena con pasas</h2>
+                        <p>Galletas de avena con pasas</p>
+                        <p>Precio<br>$50</p>
+                        <button id="postres2">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Helado de vainilla con caramelo</h2>
+                        <p>Helado de vainilla con caramelo</p>
+                        <p>Precio<br>$50</p>
+                        <button id="postres3">Agregar</button>
+                    </div>
+                </div>
+                <div id="bebidasdiv">
+                    <div class="comida">
+                        <h2>Jugo de naranja</h2>
+                        <p>Jugo de naranja</p>
+                        <p>Precio<br>$50</p>
+                        <button id="bebidas1">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Jugo de piña</h2>
+                        <p>Jugo de piña</p>
+                        <p>Precio<br>$50</p>
+                        <button id="bebidas2">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Jugo de mango</h2>
+                        <p>Jugo de mango</p>
+                        <p>Precio<br>$50</p>
+                        <button id="bebidas3">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Jugo de papaya</h2>
+                        <p>Jugo de papaya</p>
+                        <p>Precio<br>$50</p>
+                        <button id="bebidas4">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Jugo de uva</h2>
+                        <p>Jugo de uva</p>
+                        <p>Precio<br>$50</p>
+                        <button id="bebidas5">Agregar</button>
+                    </div>
+                </div>
+                <div id="extrasdiv">
+                    <div class="comida">
+                        <h2>Arroz</h2>
+                        <p>Arroz</p>
+                        <p>Precio<br>$50</p>
+                        <button id="extras1">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Papas fritas</h2>
+                        <p>Papas fritas</p>
+                        <p>Precio<br>$50</p>
+                        <button id="extras2">Agregar</button>
+                    </div>
+                    <div class="comida">
+                        <h2>Plátano frito</h2>
+                        <p>Plátano frito</p>
+                        <p>Precio<br>$50</p>
+                        <button id="extras4">Agregar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<!--Este es el menú-->
+        <div id="cuerpo">
+            <div class="table">
+                <div class="menus">
+                    <img src="imagenes/ensalada.png">
+                    <h2>Entradas</h2>
+                    <ul>
+                        <li>Ensalada de tomate y cebolla</li>
+                        <li>Ensalada de col y zanahoria</li>
+                        <li>Ensalada de lechuga y pepino</li>
+                        <li>Ensalada de espinaca y aguacate</li>
+                        <li>Ensalada de lechuga y tomate</li>
+                    </ul>
+                </div>
+                    
+                <div class="menus">
+                <img src="imagenes/ribeye.png">
+                    <h2>Platos fuertes</h2>
+                        <ul>
+                            <li>Pollo a la plancha</li>
+                            <li>Carne asada</li>
+                            <li>Filete de pescado</li>
+                            <li>Chuleta de cerdo</li>
+                            <li>Chuleta de res</li>
+                        </ul>
+                </div>
+                <div class="menus">
+                <img src="imagenes/tarta fresa.png">
+                    <h2>Postres</h2>
+                        <ul>
+                            <li>Brownie de chocolate</li>
+                            <li>Galletas de avena con pasas</li>
+                            <li>Helado de vainilla con caramelo</li>
+                        </ul>
+                </div>
+            </div>
+            <div class="table">
+            <div class="menus">
+            <img src="imagenes/jugos.png">
+                    <h2>Bebidas</h2>
+                        <ul>
+                            <li>Jugo de naranja</li>
+                            <li>Jugo de piña</li>
+                            <li>Jugo de mango</li>
+                            <li>Jugo de papaya</li>
+                            <li>Jugo de uva</li>
+                        </ul>
+            </div>
+                <div class="menus">
+                <img src="imagenes/crema.png">
+                    <h2>Extras</h2>
+                    <ul>
+                        <li>Arroz</li>
+                        <li>Papas fritas</li>
+                        <li>Plátano frito</li>
+                    </ul>
+                </div>
+
+                
+            </div>
+        </div>
     </main>
     <script>
         $(document).ready(function(){
-            $('main').load('mainmenu.php');
+            $('#main2').hide();
             $('#ordenar').click(function(){
                 if($('#ordenar').text() == 'Ordenar'){
                     $('#ordenar').text('Menú');
                     $('#titulo').text('Ordenar');
-                    $('mainmenu.php').hide(1000);
-                    $('main').load('ordenar.php');
+                    $('#cuerpo').hide();
+                    $('#main2').show();
                     $('#platosfuertesdiv').hide();
                     $('#postresdiv').hide();
                     $('#bebidasdiv').hide();
@@ -43,8 +252,8 @@
                 else{
                     $('#ordenar').text('Ordenar');
                     $('#titulo').text('Menú');
-                    $('ordenar.php').fadeOut();
-                    $('main').load('mainmenu.php').fadein();
+                    $('#main2').hide();
+                    $('#cuerpo').show();
                 }
             });
             $('#entradas').click(function(){
