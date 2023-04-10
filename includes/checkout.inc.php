e816d0c603187f0e00a7333f2c1de08ce49a092a
Yeury Paula <YFPO0001@ce.pucmm.edu.do>
2023-04-10 06:21:41 -0400

    Ya funciona COÑO

--- includes/checkout.inc.php
+++ includes/checkout.inc.php
@@ -12,17 +12,18 @@
 
     $comprobarsql = "SELECT * FROM `orden_carrito` WHERE `id_usuario` = '$usuario'";
     $comprobarresult = mysqli_query($conn, $comprobarsql);
-    $comprobarrow = mysqli_fetch_assoc($comprobarresult);
 
-    if ($comprobarrow){
+    if (mysqli_num_rows($comprobarresult) > 0) {
         $sql2 = "INSERT INTO `pedidos` (`id_usuario`, `fecha`, `estado`, `valor_total`) VALUES ('$usuario', CURRENT_DATE(), 'PENDIENTE', '$total')";
         mysqli_query($conn, $sql2);
+
+        $id_pedido = mysqli_insert_id($conn);
 
         while ($row = mysqli_fetch_assoc($result)) {
             $id_producto = $row['id_producto'];
             $cantidad = $row['cantidad'];
 
-            $sql3 = "INSERT INTO `detalle_orden` (`id_pedido`, `id_producto`, `cantidad`) VALUES ('$usuario', '$id_producto', '$cantidad')";
+            $sql3 = "INSERT INTO `detalle_orden` (`id_pedido`, `id_producto`, `cantidad`) VALUES ('$id_pedido', '$id_producto', '$cantidad')";
             mysqli_query($conn, $sql3);
         }
 
