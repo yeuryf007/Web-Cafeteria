c45ae3357e1c8ced2235a413d7cc65c4a30fb9a8
Yeury Paula <YFPO0001@ce.pucmm.edu.do>
2023-04-10 08:07:45 -0400

    arreglao

--- includes/printmenu.php
+++ includes/printmenu.php
@@ -13,7 +13,6 @@
         $nombre = $row['nombre'];
         $precio = $row['precio'];
         $descripcion = $row['descripcion'];
-        $stock = $row['stock'];
         
         echo '<div class="comida"><h2>', $nombre, '</h2><p>', $nombre, '</p><p>Precio<br>$', $precio, '</p><button class="order-button" id="', $id_producto, '">Agregar</button></div>';
     }
